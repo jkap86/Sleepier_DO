0a9c327b020dc71ff161e0f9a637de43a3b2c562
Jay Kapoor <jkap86@gmail.com>
2023-11-01 17:03:08 -0400

    add post route to update player ktc history

--- server/app/backgroundTasks/getPlayerValues.js
+++ server/app/backgroundTasks/getPlayerValues.js
@@ -102,6 +102,86 @@ module.exports = async (app) => {
         console.log(`Update Complete`)
     }
 
+    if (process.env.KTC_URL) {
+        const ktc_html = await axios.get(process.env.KTC_URL)
+
+        const regex = /<body[^>]*>[\s\S]*?<script[^>]*>([\s\S]*?)<\/script>/i;
+
+        const match = ktc_html.data.match(regex)
+
+        if (match && match[1]) {
+            const player = {
+                playerID: parseInt(process.env.KTC_URL.split('-')[process.env.KTC_URL.split('-').length - 1])
+            }
+
+            const extractArray = (variableName, content) => {
+                const regex = new RegExp(`var ${variableName} = ({.*?});`, 's');
+                const match = content.match(regex);
+                return match ? JSON.parse(match[1]) : null;
+            };
+
+            const sleeper_id = matchPlayer(player)
+
+            const sf_history_array = extractArray('playerSuperflex', match[1])
+            const oneqb_history_array = extractArray('playerOneQB', match[1])
+
+            const player_history = [];
+
+            sf_history_array.overallValue
+                .forEach(obj => {
+                    const date = getUTCDate(new Date(obj.d));
+                    const value = obj.v;
+
+                    console.log(`sf - ${date} - ${value}`)
+                    player_history.push({
+                        date: date,
+                        type: 'sf',
+                        player_id: sleeper_id,
+                        value: value
+                    })
+
+                })
+
+            oneqb_history_array.overallValue
+                .forEach(obj => {
+                    const date = getUTCDate(new Date(obj.d));
+                    const value = obj.v;
+
+                    console.log(`oneqb - ${date} - ${value}`)
+
+                    player_history.push({
+                        date: date,
+                        type: 'oneqb',
+                        player_id: sleeper_id,
+                        value: value
+                    })
+
+                })
+
+            try {
+                const history = require('../../playervalues.json')
+
+                const data = [
+                    ...history,
+                    ...player_history
+                        .filter(ph => !(
+                            history.find(h => ph.date === h.date
+                                && ph.type === h.type
+                                && ph.player_id === h.player_id
+                            )
+                        ))
+                ]
+
+                fs.writeFileSync('./playervalues.json', JSON.stringify(player_history));
+            } catch (err) {
+                console.log(err.message)
+            }
+            console.log('Script content saved to ktc.js!');
+        } else {
+            console.error('Script tag not found inside the body!');
+        }
+    }
+
     if (process.env.HEROKU) {
         await getDailyValues()
 
--- server/app/routes/dynastyrankings.routes.js
+++ server/app/routes/dynastyrankings.routes.js
@@ -11,5 +11,92 @@ module.exports = app => {
 
     router.get("/findrange", dynastyrankings.findrange)
 
+    router.post("/player_history", (req, res) => {
+        const updatePlayerValues = async (ktc_url) => {
+            const ktc_html = await axios.get(ktc_url)
+
+            const regex = /<body[^>]*>[\s\S]*?<script[^>]*>([\s\S]*?)<\/script>/i;
+
+            const match = ktc_html.data.match(regex)
+
+            if (match && match[1]) {
+                const player = {
+                    playerID: parseInt(ktc_url.split('-')[ktc_url.split('-').length - 1])
+                }
+
+                const extractArray = (variableName, content) => {
+                    const regex = new RegExp(`var ${variableName} = ({.*?});`, 's');
+                    const match = content.match(regex);
+                    return match ? JSON.parse(match[1]) : null;
+                };
+
+                const sleeper_id = matchPlayer(player)
+
+                const sf_history_array = extractArray('playerSuperflex', match[1])
+                const oneqb_history_array = extractArray('playerOneQB', match[1])
+
+                const player_history = [];
+
+                sf_history_array.overallValue
+                    .forEach(obj => {
+                        const date = getUTCDate(new Date(obj.d));
+                        const value = obj.v;
+
+                        console.log(`sf - ${date} - ${value}`)
+                        player_history.push({
+                            date: date,
+                            type: 'sf',
+                            player_id: sleeper_id,
+                            value: value
+                        })
+
+                    })
+
+                oneqb_history_array.overallValue
+                    .forEach(obj => {
+                        const date = getUTCDate(new Date(obj.d));
+                        const value = obj.v;
+
+                        console.log(`oneqb - ${date} - ${value}`)
+
+                        player_history.push({
+                            date: date,
+                            type: 'oneqb',
+                            player_id: sleeper_id,
+                            value: value
+                        })
+
+                    })
+
+                try {
+                    const history = require('../../playervalues.json')
+
+                    const data = [
+                        ...history,
+                        ...player_history
+                            .filter(ph => !(
+                                history.find(h => ph.date === h.date
+                                    && ph.type === h.type
+                                    && ph.player_id === h.player_id
+                                )
+                            ))
+                    ]
+
+                    fs.writeFileSync('./playervalues.json', JSON.stringify(player_history));
+                } catch (err) {
+                    console.log(err.message)
+                }
+                console.log('Script content saved to ktc.js!');
+            } else {
+                console.error('Script tag not found inside the body!');
+            }
+        }
+
+        const ktc_url = req.body.ktc_url;
+
+        updatePlayerValues(ktc_url)
+
+    })
+
     app.use('/dynastyrankings', router);
 }
