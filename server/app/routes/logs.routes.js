97af377355ccc4a7db611d364211f522e5f52bfc
Jay Kapoor <jkap86@gmail.com>
2023-11-03 14:20:31 -0400

    -

---
+++ server/app/routes/logs.routes.js
@@ -0,0 +1,83 @@
+'use strict'
+
+const fs = require('fs');
+const path = require('path');
+
+
+module.exports = (app) => {
+    var router = require("express").Router();
+
+    router.get('/users/:date', (req, res) => {
+        console.log('Getting logs of users');
+
+        const logs = fs.readFileSync('./logs.json')
+
+        const logs_to_send = []
+
+        JSON.parse(logs)
+            .filter(l => req.params.date === l.timestamp.split(',')[0].replace(/[^0-9]/g, '') && (
+                l.route.includes('/user/create?username=')
+            ))
+            .forEach(entry => {
+                const entry_username = entry.request?.username?.toLowerCase()
+
+                if (entry_username) {
+                    const existing_log_to_send = logs_to_send.find(lts => lts.username === entry_username)
+                    if (!existing_log_to_send) {
+                        logs_to_send.push({
+                            username: entry_username,
+                            searches: [{ ip_address: entry.ip, timestamp: entry.timestamp }]
+                        })
+                    } else {
+                        existing_log_to_send.searches.push({
+                            ip_address: entry.ip,
+                            timestamp: entry.timestamp
+                        })
+                    }
+                }
+            })
+
+        res.send({
+            total_users_searched: logs_to_send.length,
+            total_searches: logs_to_send.flatMap(lts => lts.searches)?.length,
+            searches: logs_to_send.sort((a, b) => b.searches.length - a.searches.length)
+        })
+    })
+
+    router.get('/ip/:date', (req, res) => {
+        console.log('Getting logs of users');
+
+        const logs = fs.readFileSync('./logs.json')
+
+        const logs_to_send = []
+
+        JSON.parse(logs)
+            .filter(l => req.params.date === l.timestamp.split(',')[0].replace(/[^0-9]/g, ''))
+            .forEach(entry => {
+                const entry_ip = entry.ip
+
+                if (entry_ip) {
+                    const existing_log_to_send = logs_to_send.find(lts => lts.ip_address === entry_ip)
+                    if (!existing_log_to_send) {
+                        logs_to_send.push({
+                            ip_address: entry_ip,
+                            searches: [{ ip_address: entry.ip, timestamp: entry.timestamp }]
+                        })
+                    } else {
+                        existing_log_to_send.searches.push({
+                            ip_address: entry.username,
+                            timestamp: entry.timestamp
+                        })
+                    }
+                }
+            })
+
+        res.send({
+            total_IPAddresses: logs_to_send.length,
+            total_searches: logs_to_send.flatMap(lts => lts.searches)?.length,
+            searches: logs_to_send.sort((a, b) => b.searches.length - a.searches.length)
+        })
+    })
+
+    app.use('/logs', router);
+}
--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ function start() {
     app.use(express.json({ limit: '50mb' }));
     app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-    if (process.env.HEROKU) {
+    if (process.env.DATABASE_URL) {
         app.use(logVisits);
     }
 
@@ -48,7 +48,7 @@ function start() {
     require('./app/routes/league.routes')(app);
     require('./app/routes/trade.routes')(app);
     require('./app/routes/ringOfFire.routes')(app);
-    require('./app/routes/dynastyrankings.routes')(app)
+    require('./app/routes/logs.routes')(app)
 
     app.get('*', async (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'));
